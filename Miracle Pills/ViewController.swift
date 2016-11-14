f7c248c9860b0d1af63549cf354fbcaebd7abe91
Mark Rabins <markrabins@gmail.com>
2016-11-13 22:22:02 -0800

    Initial commit

---
+++ Miracle Pills/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Miracle Pills
+//
+//  Created by Mark Rabins on 11/12/16.
+//  Copyright © 2016 self.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerButton: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func stateButtonPressed(sender: UIButton) {
+        
+    }
+    
+    
+}
+
